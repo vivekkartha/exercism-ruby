4855448d0a81bf1636553768301cc8d7e462c668
Vivek Kartha <vivek.kartha@mobme.in>
2018-09-27 03:02:56 +0530

    Refactored with ternary

--- two-fer/two_fer.rb
+++ two-fer/two_fer.rb
@@ -1,9 +1,5 @@
 class TwoFer
     def self.two_fer(*args)
-        if args.empty?
-            "One for you, one for me."
-        else 
-            "One for #{args.first}, one for me."
-        end
+        "One for #{args.empty? ? "you" : args.first}, one for me."  
     end
 end
